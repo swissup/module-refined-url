123bdeabd74bf3bd32621058c413280fe145a685
Vitaliy <vitaliy@mage2cloud.com>
2018-12-18 14:46:19 +0200

    Force product url path to fit in `url_rewrite`.`request_path` field

---
+++ Helper/Data.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Swissup\RefinedUrl\Helper;
+
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\CatalogUrlRewrite\Model\ProductUrlPathGenerator;
+use Magento\UrlRewrite\Model\UrlFinderInterface;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
+
+class Data extends AbstractHelper
+{
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    protected $resource;
+
+    /**
+     * @var int
+     */
+    protected $requestPathLength = null;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param UrlFinderInterface                        $urlFinder
+     * @param Context                                   $context
+     */
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resource,
+        UrlFinderInterface $urlFinder,
+        Context $context
+    ) {
+        $this->resource = $resource;
+        $this->urlFinder = $urlFinder;
+        parent::__construct($context);
+    }
+
+    /**
+     * Get max length for request_path field in table `url_rewrite`
+     *
+     * @return int
+     */
+    public function getRequestPathMaxLength()
+    {
+        if (!isset($this->requestPathLength)) {
+            $fields = $this->resource->getConnection()->describeTable(
+                $this->resource->getTableName('url_rewrite')
+            );
+            $requestPath = isset($fields['request_path'])
+                ? $fields['request_path']
+                : null;
+            if ($requestPath && $requestPath['LENGTH']) {
+                $this->requestPathLength = (int)$requestPath['LENGTH'];
+            } else {
+                $this->requestPathLength = 0;
+            }
+        }
+
+        return $this->requestPathLength;
+    }
+
+    /**
+     * Get product url sufix from Magento config
+     *
+     * @param  int    $storeId
+     * @return string
+     */
+    public function getProductUrlSuffix($storeId)
+    {
+        return $this->scopeConfig->getValue(
+            ProductUrlPathGenerator::XML_PATH_PRODUCT_URL_SUFFIX,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    /**
+     * @param string $requestPath
+     * @param int $storeId
+     * @return UrlRewrite|null
+     */
+    public function getRewrite($requestPath, $storeId)
+    {
+        return $this->urlFinder->findOneByData([
+            UrlRewrite::REQUEST_PATH => ltrim($requestPath, '/'),
+            UrlRewrite::STORE_ID => $storeId,
+        ]);
+    }
+}
---
+++ Plugin/CatalogUrlRewrite/Model/ProductUrlPathGenerator.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Swissup\RefinedUrl\Plugin\CatalogUrlRewrite\Model;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class ProductUrlPathGenerator
+{
+    /**
+     * @var \Swissup\RefinedUrl\Helper\Data
+     */
+    private $helper;
+
+    /**
+     * @param \Swissup\RefinedUrl\Helper\Data $helper
+     */
+    public function __construct(
+        \Swissup\RefinedUrl\Helper\Data $helper
+    ) {
+        $this->helper = $helper;
+    }
+
+    /**
+     * Check max allowed url length
+     *
+     * @param  \Magento\CatalogUrlRewrite\Model\ProductUrlPathGenerator $subject
+     * @param  string                                                   $result
+     * @param  \Magento\Catalog\Model\Product                           $product
+     * @param  int                                                      $storeId
+     * @param  \Magento\Catalog\Model\Category|null                     $category
+     * @return string
+     */
+    public function afterGetUrlPathWithSuffix(
+        \Magento\CatalogUrlRewrite\Model\ProductUrlPathGenerator $subject,
+        $result,
+        \Magento\Catalog\Model\Product $product,
+        $storeId,
+        \Magento\Catalog\Model\Category $category = null
+    ) {
+        $maxLength = $this->helper->getRequestPathMaxLength();
+        if ($maxLength && strlen($result) > $maxLength) {
+            $shortedPath = $this->generateShortedPath($result, $storeId);
+            if ($shortedPath === null) {
+                throw new LocalizedException(
+                    __(
+                        "<b>Generated URL is too long!</b>\nEntity type - \"%1\".\nEntity ID - \"%2\".<br />\nGenerated URL path \"%3\" has %4 characters.\nMax allowed length is %5.",
+                        'product',
+                        $product->getId(),
+                        $result,
+                        strlen($result),
+                        $maxLength
+                    )
+                );
+            }
+
+            return $shortedPath;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Generate shorted out url path from original one
+     *
+     * @param  string      $originalPath
+     * @param  int         $storeId
+     * @return string|null
+     */
+    public function generateShortedPath($originalPath, $storeId)
+    {
+        $urlSufix = $this->helper->getProductUrlSuffix($storeId);
+        $length = $this->helper->getRequestPathMaxLength();
+        $appendSufix = false;
+        if ($urlSufix
+            && substr($originalPath, -strlen($urlSufix)) === $urlSufix // ends with $urlSufix
+        ) {
+            $length -= strlen($urlSufix);
+            $appendSufix = true;
+        }
+
+        $i = 0;
+        do {
+            $i++;
+            $shortedPath = substr($originalPath, 0, $length - 1 - strlen((string)$i))
+                . '~'
+                . (string)$i
+                . ($appendSufix ? $urlSufix : '');
+            // check if exists rewrite for shorted path
+            if ($this->helper->getRewrite($shortedPath, $storeId)) {
+                $shortedPath = '';
+            }
+        } while (empty($shortedPath) && $i <= 99);
+
+        return empty($shortedPath) ? null : $shortedPath;
+    }
+}
